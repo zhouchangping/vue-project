ad9d3ccb522d7a7da7c72bcfc0f595a8318f096d
周昌平 <zhouchangping@zhouchangpingdeMacBook-Pro.local>
2020-09-01 17:06:55 +0800

    feat:测试

--- README.md
+++ README.md
@@ -20,3 +20,12 @@ npm run build --report
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 # vue-project
+
+## commit-message
++ feat：新功能（feature）
++ fix：修补bug
++ docs：文档（documentation）
++ style： 格式（不影响代码运行的变动）
++ refactor：重构（即不是新增功能，也不是修改bug的代码变动）
++ test：增加测试
++ chore：构建过程或辅助工具的变动
--- hooks/check-commit-msg.js
+++ hooks/check-commit-msg.js
@@ -5,7 +5,7 @@ const path = require("path");
 const msgPath = process.argv.slice(2)[0] ?
   path.join(__dirname, "..", process.argv.slice(2)[0]):
   path.join(__dirname, "..", ".git", "COMMIT_EDITMSG");
-
+console.log(msgPath + "-------");
 let msgContent = fs.readFileSync(msgPath, "utf-8").toString();
 // 移除注视
 msgContent = msgContent.replace(/^#.+/mg, "");
@@ -15,18 +15,27 @@ if (/^[Mm]erge/.test(msgContent)) {
 }
 
 const TYPES = [
-  "feat"
+  "feat",
+  "fix",
+  "lint",
+  "docs",
+  "test",
+  "ui",
+  "native",
+  "style"
 ];
 
 // commit message校验正则
-const syntaxRe = /^(\W+)(\(.+\))?: ?(\S.*)/;
+const syntaxRe = /^(\w+)(\(.+\))?: ?(\S.*)/;
 
 
 const match = msgContent.match(syntaxRe);
+console.log(match+"-------");
 
-let pass = ture;
+let pass = true;
 if (match) {
   const [_, type, scope, subject] = match;
+  console.oog(type+"---"+scope+"---"+subject);
   if (!TYPES.includes(type)) {
     pass = false;
     console.log("butongg");
