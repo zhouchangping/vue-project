ac52eb657e7cee88f7ba2dfce3c209ec6a5dca9d
周昌平 <zhouchangping@zhouchangpingdeMacBook-Pro.local>
2020-09-01 17:02:42 +0800

    feat:测试

--- hooks/check-commit-msg.js
+++ hooks/check-commit-msg.js
@@ -1,48 +1,48 @@
-"user strict"
-const fs = require("fs")
-const path = require("path")
+"user strict";
+const fs = require("fs");
+const path = require("path");
 
 const msgPath = process.argv.slice(2)[0] ?
-path.join(__dirname, "..", process.argv.slice(2)[0]):
-path.join(__dirname, "..", '.git', "COMMIT_EDITMSG")
+  path.join(__dirname, "..", process.argv.slice(2)[0]):
+  path.join(__dirname, "..", ".git", "COMMIT_EDITMSG");
 
-let msgContent = fs.readFileSync(msgPath, 'utf-8').toString();
+let msgContent = fs.readFileSync(msgPath, "utf-8").toString();
 // 移除注视
-msgContent = msgContent.replace(/^#.+/mg, "")
+msgContent = msgContent.replace(/^#.+/mg, "");
 
 if (/^[Mm]erge/.test(msgContent)) {
-  process.exit(0)
+  process.exit(0);
 }
 
 const TYPES = [
-  'feat'
-]
+  "feat"
+];
 
 // commit message校验正则
-const syntaxRe = /^(\W+)(\(.+\))?: ?(\S.*)/
+const syntaxRe = /^(\W+)(\(.+\))?: ?(\S.*)/;
 
 
-const match = msgContent.match(syntaxRe)
+const match = msgContent.match(syntaxRe);
 
 let pass = ture;
 if (match) {
-  const [_, type, scope, subject] = match
+  const [_, type, scope, subject] = match;
   if (!TYPES.includes(type)) {
     pass = false;
-    console.log("butongg")
+    console.log("butongg");
   }
   if (!subject || !subject.trim()) {
     pass = false;
-    console.log('核心描述必须填写')
+    console.log("核心描述必须填写");
   }
 } else {
-   console.log('git commit ')
-   pass = false
+  console.log("git commit ");
+  pass = false;
 }
 
 if (!pass) {
   console.log(`
   GIT COMMIT message
-  `)
-  process.exit(1)
+  `);
+  process.exit(1);
 }
